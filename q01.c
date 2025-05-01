995a94d37939e5876910f3187c2a691757568418
rrodriguess20 <rodriguess.rebeca20@gmail.com>
2025-04-30 21:04:03 -0300

    Alterações no código

---
+++ q01.c
@@ -0,0 +1,61 @@
+/*Escreva uma função que conta o nó de células de uma lista simplesmente encadeada. Apresente a
+implementação para uma lista encadeada sem cabeça. Apresente esta implementação para uma lista
+encadeada com cabeça.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula{
+
+	int info;
+
+	struct celula * prox;
+
+}Celula;
+
+typedef Celula * CCelula;
+
+CCelula criaCelula(){
+
+	return NULL;
+}
+
+CCelula insereElemento(CCelula lista, int i){
+
+	CCelula novo =  (CCelula)malloc(sizeof(Celula));
+
+	novo->info = i;
+	novo->prox = lista;
+	return novo;
+}
+
+void contaCelulas(CCelula lista){
+
+	int count = 0;
+	CCelula listaAuxiliar = (CCelula)malloc(sizeof(Celula));
+	listaAuxiliar = lista;
+
+	while(listaAuxiliar!=NULL){
+
+		count++;
+		listaAuxiliar = listaAuxiliar->prox;
+	}
+
+	printf("O numero de elementos na lista e: %d\n", count);
+}
+
+int main(){
+
+	CCelula teste;
+	teste = criaCelula();
+
+	teste = insereElemento(teste, 1);
+	teste = insereElemento(teste, 2);
+	teste = insereElemento(teste, 3);
+	teste = insereElemento(teste, 4);
+	teste = insereElemento(teste, 5);
+
+	contaCelulas(teste);
+
+	return 0;
+}	
